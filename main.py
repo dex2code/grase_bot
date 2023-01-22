4a8401c0104f424e53ca6a235ff6e1e3475bf307
dexen <dexen@phoenix.local>
2023-01-22 14:05:36 +0300

    commit

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-import telebot
-import requests
 import os
 import sys
+from datetime import datetime
+
+import requests
+import telebot
 import translators.server as tss
 import wikipediaapi
-from datetime import datetime
-from loguru import logger
 from dotenv import load_dotenv
+from loguru import logger
 
 
-'''
+"""
+Добавляем лог-файл
+"""
+logger.add("main.log", format="{time} -- {level} -- {message}", level="DEBUG", rotation="1 week", compression="zip")
+
+
+"""
 Подгружаем токен из .env и стартуем бота
-'''
+"""
 try:
-    load_dotenv(".env")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    bot = telebot.TeleBot(BOT_TOKEN)
+    load_dotenv(dotenv_path=".env")
+    BOT_TOKEN = os.getenv(key="BOT_TOKEN")
+    bot = telebot.TeleBot(token=BOT_TOKEN)
 except Exception as E:
     logger.error(f"Ошибка при старте бота: 'str({E})'")
     sys.exit()
@@ -28,9 +35,11 @@ logger.info(f"Стартовали бота: {bot}")
 """
 try:
     bot.set_my_commands([
-        telebot.types.BotCommand("/help", "Команды бота"),
-        telebot.types.BotCommand("/day",  "События дня"),
-        telebot.types.BotCommand("/wiki", "Узнать значение в WikiPedia")
+        telebot.types.BotCommand(command="/help",   description="Команды бота"),
+        telebot.types.BotCommand(command="/day",    description="События дня"),
+        telebot.types.BotCommand(command="/wiki",   description="Узнать значение в WikiPedia"),
+        telebot.types.BotCommand(command="/tr_rus", description="Перевести на Русский"),
+        telebot.types.BotCommand(command="/tr_eng", description="Перевести на English")
     ])
 except Exception as E:
     logger.error(f"Ошибка при установке меню бота: 'str({E})'")
@@ -43,7 +52,7 @@ logger.info(f"Установили команды бота.")
 Подключаем Wikipedia
 """
 try:
-    wiki = wikipediaapi.Wikipedia('ru')
+    wiki = wikipediaapi.Wikipedia(language="ru")
 except Exception as E:
     logger.error(f"Ошибка при подключении Wikipedia: 'str({E})'")
     sys.exit()
@@ -61,8 +70,11 @@ def bot_start(message):
     """
     logger.info(f"Получен запрос '{message.text}' от пользователя '{message.from_user.id}'")
 
-    answer = f"Привет, <b>{message.from_user.first_name}</b> ✌ \n\nЧтобы узнать все команды бота: /help\n"
-    bot.send_message(chat_id=message.chat.id, text=answer, parse_mode='html')
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=f"Привет, <b>{message.from_user.first_name}</b> ✌ \n\nЧтобы узнать все команды бота: /help\n",
+        parse_mode="html"
+    )
 
 
 @bot.message_handler(commands=["help"])
@@ -86,7 +98,7 @@ def bot_help(message):
 
     Связаться с создателем бота: @kmmax
     """
-    bot.send_message(chat_id=message.chat.id, text=answer, parse_mode='html')
+    bot.send_message(chat_id=message.chat.id, text=answer, parse_mode="html")
 
 
 @bot.message_handler(commands=["day"])
@@ -97,17 +109,17 @@ def day(message):
     """
     logger.info(f"Получен запрос '{message.text}' от пользователя '{message.from_user.id}'")
 
-    cur_date = datetime.now()
-
-    cur_day   = str(cur_date.day).zfill(2)
-    cur_month = str(cur_date.month).zfill(2)
+    cur_date  = datetime.now()
+    cur_day   = str(object=cur_date.day).zfill(2)
+    cur_month = str(object=cur_date.month).zfill(2)
 
     api_url = f"http://numbersapi.com/{cur_month}/{cur_day}/date"
 
     try:
         api_res = requests.get(url=api_url, timeout=10)
-    except Exception:
+    except Exception as E:
         api_res_text_eng = "<b>Ошибка!</b> Сервер недоступен!"
+        logger.error(f"Ошибка при получении данных от сервиса: ({str(E)})")
     else:
         if api_res and api_res.status_code == 200:
             api_res_text_eng = api_res.text
@@ -115,16 +127,29 @@ def day(message):
             api_res_text_eng = f"<b>Ошибка!</b> Сервер вернул ошибку ({str(api_res.status_code)})!"
 
     try:
-        api_res_text_rus = tss.google(query_text=api_res_text_eng, to_language='ru')
-    except Exception:
+        api_res_text_rus = tss.google(query_text=api_res_text_eng, to_language="ru")
+    except Exception as E:
         api_res_text_rus = ""
+        logger.error(f"Ошибка при переводе строки: ({str(E)})")
 
     if api_res_text_rus:
-        answer = f"Вот интересный факт про сегодняшний день: <i>" + api_res_text_rus + "</i>\n /day "
-    else:
-        answer = f"Вот интересный факт про сегодняшний день (к сожалению, перевод временно не работает): <i>" + api_res_text_eng  + "</i>\n /day "
+        answer = f"""
+        Вот интересный факт про сегодняшний день:
 
-    bot.send_message(chat_id=message.chat.id, text=answer, parse_mode='html')
+        <i>{api_res_text_rus}</i>
+
+        Еще один факт: /day
+        """
+    else:
+        answer = f"""
+        Вот интересный факт про сегодняшний день (к сожалению, перевод временно не работает):
+
+        <i>{api_res_text_eng}</i>
+
+        Еще один факт: /day
+        """
+
+    bot.send_message(chat_id=message.chat.id, text=answer, parse_mode="html")
 
 
 @bot.message_handler(commands=["wiki"])
@@ -133,10 +158,15 @@ def bot_wiki(message):
     """
     Функция Wiki - регистрирует обработчик для следующего введенного слова
     """
-    logger.info(f"{datetime.now()} - Получен запрос '{message.text}' от пользователя '{message.from_user.id}'")
+    logger.info(f"Получен запрос '{message.text}' от пользователя '{message.from_user.id}'")
 
-    user_msg = bot.send_message(message.chat.id, "Введите слово, значение которого вы хотите найти в Wikipedia:" )
-    bot.register_next_step_handler(user_msg, bot_wiki_parse)
+    user_msg = bot.send_message(
+        chat_id=message.chat.id,
+        text="Введите слово, значение которого вы хотите найти в <b>Wikipedia</b>:",
+        parse_mode="html"
+    )
+
+    bot.register_next_step_handler(message=user_msg, callback=bot_wiki_parse)
 
 @logger.catch
 def bot_wiki_parse(message):
@@ -145,22 +175,114 @@ def bot_wiki_parse(message):
     """
     logger.info(f"Получен запрос '{message.text}' от пользователя '{message.from_user.id}'")
 
-    user_request = str(message.text)
+    user_request = str(object=message.text)
     user_request = user_request.strip()
 
-    bot.send_message(message.chat.id, f"Вы хотели найти '<i>{user_request}</i>'", parse_mode='html')
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=f"Вы хотите найти: '<i>{user_request}</i>'",
+        parse_mode="html"
+    )
 
-    wiki_page = wiki.page(user_request)
+    wiki_page = wiki.page(title=user_request)
 
     if wiki_page.exists():
         page_sum = wiki_page.summary[0:1024]
         page_sum = page_sum.split(" ")[:-1]
         page_sum = " ".join(page_sum) + "..."
-        bot.send_message(message.chat.id, f"{page_sum}", parse_mode='html')
-        bot.send_message(message.chat.id, f"Узнать больше тут: {wiki_page.fullurl}", parse_mode='html')
-        bot.send_message(message.chat.id, f"Еще один запрос: /wiki", parse_mode='html')
+        bot.send_message(chat_id=message.chat.id, text=f"{page_sum}", parse_mode="html")
+        bot.send_message(chat_id=message.chat.id, text=f"Узнать больше тут: {wiki_page.fullurl}", parse_mode="html")
+        bot.send_message(chat_id=message.chat.id, text=f"Еще один запрос: /wiki", parse_mode="html")
     else:
-        bot.send_message(message.chat.id, f"К сожалению, по вашему запросу ничего не найдено. Попробуйте указать другое слово, после команды /wiki", parse_mode='html')
+        bot.send_message(
+            chat_id=message.chat.id,
+            text=f"К сожалению, по вашему запросу ничего не найдено. Попробуйте указать другое слово после команды /wiki",
+            parse_mode="html"
+        )
+
+
+@bot.message_handler(commands=["tr_rus"])
+@logger.catch
+def bot_tr_rus(message):
+    """
+    Функция Translate to Russian - регистрирует обработчик для следующего введенного слова
+    """
+    logger.info(f"Получен запрос '{message.text}' от пользователя '{message.from_user.id}'")
+
+    user_msg = bot.send_message(
+        chat_id=message.chat.id,
+        text="Введите слово или фразу, которую вы хотите перевести <b>на Русский язык</b>:",
+        parse_mode="html"
+    )
+
+    bot.register_next_step_handler(message=user_msg, callback=bot_tr_rus_parse)
+
+@logger.catch
+def bot_tr_rus_parse(message):
+    """
+    Функция Translate to Russian Parse - переводит слово и пишет в чат полученный перевод
+    """
+    logger.info(f"Получен запрос '{message.text}' от пользователя '{message.from_user.id}'")
+
+    user_request = str(object=message.text)
+    user_request = user_request.strip()
+
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=f"Вы хотите перевести <b>на Русский язык</b>: '<i>{user_request}</i>'",
+        parse_mode="html"
+    )
+
+    try:
+        translated_rus = tss.google(query_text=user_request, to_language="ru")
+    except Exception as E:
+        bot.send_message(chat_id=message.chat.id, text=f"К сожалению, перевод временно не работает.\n Попробуйте еще раз: /tr_rus", parse_mode="html")
+        logger.error(f"Ошибка при переводе строки: ({str(E)})")
+
+    bot.send_message(chat_id=message.chat.id, text=f"Перевод: <i>'{translated_rus}'</i>", parse_mode="html")
+    bot.send_message(chat_id=message.chat.id, text=f"Перевести еще: <b>/tr_rus</b>", parse_mode="html")
+
+
+@bot.message_handler(commands=["tr_eng"])
+@logger.catch
+def bot_tr_eng(message):
+    """
+    Функция Translate to English - регистрирует обработчик для следующего введенного слова
+    """
+    logger.info(f"Получен запрос '{message.text}' от пользователя '{message.from_user.id}'")
+
+    user_msg = bot.send_message(
+        chat_id=message.chat.id,
+        text="Введите слово или фразу, которую вы хотите перевести <b>на English</b>:",
+        parse_mode="html"
+    )
+
+    bot.register_next_step_handler(message=user_msg, callback=bot_tr_eng_parse)
+
+@logger.catch
+def bot_tr_eng_parse(message):
+    """
+    Функция Translate to English Parse - переводит слово и пишет в чат полученный перевод
+    """
+    logger.info(f"Получен запрос '{message.text}' от пользователя '{message.from_user.id}'")
+
+    user_request = str(object=message.text)
+    user_request = user_request.strip()
+
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=f"Вы хотите перевести <b>на English</b>: '<i>{user_request}</i>'",
+        parse_mode="html"
+    )
+
+    try:
+        translated_eng = tss.google(query_text=user_request, to_language="en")
+    except Exception as E:
+        bot.send_message(chat_id=message.chat.id, text=f"К сожалению, перевод временно не работает.\n Попробуйте еще раз: /tr_eng", parse_mode="html")
+        logger.error(f"Ошибка при переводе строки: ({str(E)})")
+
+    bot.send_message(chat_id=message.chat.id, text=f"Перевод: <i>'{translated_eng}'</i>", parse_mode="html")
+    bot.send_message(chat_id=message.chat.id, text=f"Перевести еще: <b>/tr_eng</b>", parse_mode="html")
 
 
 if __name__ == "__main__":
